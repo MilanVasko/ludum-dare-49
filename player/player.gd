44891179a6924b7f855a8005235a0c8798a33fe6
Milan Vaško <m.vasko2@gmail.com>
2021-10-02 11:38:22 +0200

    Initial commit

---
+++ player/player.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+# credit goes to https://kidscancode.org/godot_recipes/2d/car_steering/
+
+var wheel_base: float = 200
+var steering_angle: float = 15
+var engine_power: float = 7000
+var friction: float = -0.9
+var drag: float = -0.001
+var braking: float = 4000
+var max_speed_reverse: float = 1000
+var slip_speed: float = 2000
+var traction_fast: float = 0.1
+var traction_slow: float = 0.7
+
+var acceleration := Vector2.ZERO
+var velocity := Vector2.ZERO
+var steer_direction := 0.0
+
+func _physics_process(delta: float) -> void:
+	acceleration = Vector2.ZERO
+	get_input()
+	apply_friction()
+	calculate_steering(delta)
+	velocity += acceleration * delta
+	velocity = move_and_slide(velocity)
+
+func get_input() -> void:
+	var turn = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
+	steer_direction = turn * deg2rad(steering_angle)
+	var forward_direction := Input.get_action_strength("accelerate") - Input.get_action_strength("brake")
+	var acceleration_speed := engine_power if forward_direction > 0.0 else braking
+	acceleration = transform.x * forward_direction * acceleration_speed
+
+func apply_friction() -> void:
+	if velocity.length() < 5:
+		velocity = Vector2.ZERO
+	var friction_force = velocity * friction
+	var drag_force = velocity * velocity.length() * drag
+	acceleration += drag_force + friction_force
+
+func calculate_steering(delta: float) -> void:
+	var rear_wheel = position - transform.x * wheel_base / 2.0
+	var front_wheel = position + transform.x * wheel_base / 2.0
+	rear_wheel += velocity * delta
+	front_wheel += velocity.rotated(steer_direction) * delta
+	var traction = traction_fast if velocity.length() > slip_speed else traction_slow
+
+	var new_heading = (front_wheel - rear_wheel).normalized()
+	var d = new_heading.dot(velocity.normalized())
+	if d > 0:
+		velocity = velocity.linear_interpolate(new_heading * velocity.length(), traction * delta)
+	elif d < 0:
+		velocity = -new_heading * min(velocity.length(), max_speed_reverse)
+	rotation = new_heading.angle()
