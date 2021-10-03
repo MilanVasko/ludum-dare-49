acb4ef609d21d6463a22d801adc63889d31a464e
Milan Vaško <m.vasko2@gmail.com>
2021-10-03 17:10:53 +0200

    Add explosion sounds

--- traffic/car.gd
+++ traffic/car.gd
@@ -1,8 +1,20 @@
 extends RigidBody2D
 
+var explosions = [
+	preload("res://traffic/sounds/explosion0.wav"),
+	preload("res://traffic/sounds/explosion1.wav"),
+	preload("res://traffic/sounds/explosion2.wav"),
+	preload("res://traffic/sounds/explosion3.wav"),
+	preload("res://traffic/sounds/explosion4.wav")
+]
+
 export(float) var player_impact_coefficient: float
+onready var explosion_player: AudioStreamPlayer2D = $ExplosionPlayer2D
 
 signal blown_up
+
+func _ready():
+	randomize()
 
 func _on_target_collided(collision: KinematicCollision2D) -> void:
 	blow_up(collision)
@@ -15,6 +27,8 @@ func blow_up(collision: KinematicCollision2D) -> void:
 	apply_central_impulse(-collision.normal * player_impact_coefficient)
 	emit_signal("blown_up")
 	$Sprite.texture = preload("res://traffic/car_damaged.png")
+	explosion_player.stream = explosions[randi() % explosions.size()]
+	explosion_player.play()
 
 func _on_body_entered(body: Node) -> void:
 	if body.has_method("collided_with_blown_up_traffic_car"):
