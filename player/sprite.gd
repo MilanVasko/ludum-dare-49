785cb842ab8ad3463014b11e5a2a2f10bf4ce2a0
Milan Vaško <m.vasko2@gmail.com>
2021-10-03 21:31:36 +0200

    New player car

--- player/sprite.gd
+++ player/sprite.gd
@@ -1,10 +1,11 @@
-extends Sprite
+extends Node2D
 
 export(Color) var immortality_color: Color
 export(Color) var mortality_color: Color
+onready var background := $Background
 
 func _on_driver_immortality_started():
-	modulate = immortality_color
+	background.modulate = immortality_color
 
 func _on_driver_immortality_ended():
-	modulate = mortality_color
+	background.modulate = mortality_color
