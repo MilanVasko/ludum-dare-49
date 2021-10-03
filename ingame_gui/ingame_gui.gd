9dd9052ff6a530679e1ff944a22fdc454a5bca87
Milan Vaško <m.vasko2@gmail.com>
2021-10-03 17:49:10 +0200

    Add winning and losing sounds

--- ingame_gui/ingame_gui.gd
+++ ingame_gui/ingame_gui.gd
@@ -1,5 +1,15 @@
 extends Control
 
+var win_sounds = [
+	preload("res://ingame_gui/sounds/win1.wav")
+]
+
+var lose_sounds = [
+	preload("res://ingame_gui/sounds/lose1.wav"),
+	preload("res://ingame_gui/sounds/lose2.wav"),
+	preload("res://ingame_gui/sounds/lose3.wav")
+]
+
 onready var aborting_container := $Aborting
 onready var aborting_seconds_remaining := $Aborting/SecondsRemaining
 onready var download_progress_bar := $DownloadProgressBar
@@ -8,6 +18,7 @@ onready var download_succeeded := $DownloadSucceeded
 onready var player_died := $PlayerDied
 onready var health_label := $Health/Label
 onready var pause_menu := $PauseMenu
+onready var audio_player := $AudioStreamPlayer
 
 func _ready() -> void:
 	aborting_container.visible = true
@@ -34,13 +45,16 @@ func _on_current_download_time_advanced(current_download_time_seconds: float, to
 	download_progress_bar.value = (current_download_time_seconds / total_download_time_seconds) * download_progress_bar.max_value
 
 func _on_download_finished() -> void:
+	play_win_sound()
 	download_succeeded.visible = true
 	download_progress_bar.value = download_progress_bar.max_value
 
 func _on_download_failed() -> void:
+	play_lose_sound()
 	download_failed.visible = true
 
 func _on_player_died() -> void:
+	play_lose_sound()
 	player_died.visible = true
 	set_download_active(false)
 
@@ -57,6 +71,14 @@ func _on_retry_pressed() -> void:
 func _on_player_health_changed(health: int, _full_health: int) -> void:
 	health_label.text = str(health)
 
-func _on_main_menu_pressed():
+func _on_main_menu_pressed() -> void:
 	var err = SceneSwitcher.switch("res://main_menu/main_menu.tscn")
 	assert(err == OK)
+
+func play_win_sound() -> void:
+	audio_player.stream = win_sounds[randi() % win_sounds.size()]
+	audio_player.play()
+
+func play_lose_sound() -> void:
+	audio_player.stream = lose_sounds[randi() % lose_sounds.size()]
+	audio_player.play()
